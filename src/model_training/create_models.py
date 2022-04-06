5770d70b53a1eee80b3e35eb1006ca3df1f02203
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-04-06 13:11:09 +0300

    some base_model_name change

--- src/model_training/create_models.py
+++ src/model_training/create_models.py
@@ -4,21 +4,35 @@ from setup import setup
 from utility import get_base_model_name
 
 def get_base_models():
+    """
+    Fetches all fine-tuned base-models from OpenAI,
+    and returns a list of models' hyperparams.
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+    models (list[dict[str, Any]]): List of models with shape [{ id: model_openai_id, params: model_hyper_params }]
+    """
     tune_param_options = get_random_param_options(type="FINE_TUNE", get_all=True)
     # Base model names derived from hyperparameter options
     base_model_names = [
-        get_base_model_name(params)
+        (get_base_model_name(params), params)
         for params in tune_param_options
     ]
     
     fine_tuned_models = openai.FineTune.list()["data"]
     models = []
     # Check that a fine tuned model can be found for all given parameter combinations
-    for name in base_model_names:
+    for (name, params) in base_model_names:
         # Check that name matches with a fine-tuned model
         model = next((m for m in fine_tuned_models if name in m["fine_tuned_model"]), None)
         assert model is not None, f"Model {name} was not found in OpenAI. You need to fine tune the model first."
-        models.append(model)
+        models.append({
+            "params": params,
+            "model_id": model["fine_tuned_model"]
+        })
     return models
     
 
@@ -39,7 +53,8 @@ def create_models():
     models (list[Any]): list of the created models
     """
     fine_tuned_models = get_base_models()
-    print(fine_tuned_models)
+    for m in fine_tuned_models:
+        print(m)
 
 setup()
 create_models()
