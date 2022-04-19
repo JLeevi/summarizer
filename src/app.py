92bf9d79854ed3820c6a020304451dd102ac411d
Leevi Jäkälä <jakala.leevi@gmail.com>
2022-04-19 15:02:11 +0300

    summarization with length constraints and dynamic prompt choice

--- src/app.py
+++ src/app.py
@@ -12,15 +12,8 @@ def create_summary():
     summary = summarizer.summarize(text)
     st.session_state.summary = summary
 
-with st.sidebar:
-    add_radio = st.radio(
-        "Select summarization style",
-        ("🤖 Deterministic", "🌶 Spicy")
-    )
-
 st.text_area("Text to summarize", key="text", height=400, placeholder="Write here...")
 st.button("Create summary", on_click=create_summary)
-
 
 if "summary" in st.session_state:
     st.write(st.session_state["summary"])
